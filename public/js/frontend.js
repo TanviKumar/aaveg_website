bfcafd23db3b0f2af49c61d240fecf0308462d8b
Tanvi Kumar <tanvikumar777@gmail.com>
2018-01-14 11:46:04 +0530

    Add static rock for mobile size

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -40,15 +40,14 @@ function draw() {
     noStroke();
 	rect(0,0,windowWidth, windowHeight);
 	background(bg_image);
-	if(iterations*20 < windowHeight*3/5 && windowHeight > 640 && windowWidth > 360) {
+	if(iterations*20 < windowHeight*3/5 && windowHeight > 750 && windowWidth > 420) {
 		image(full_rock, windowWidth/2 - windowWidth/8, iterations*20, windowWidth/4, windowHeight/3);
 		
 	}
-	else if (iterations*20 >= windowHeight*3/5  && bounced == false && complete == false && windowHeight > 640 && windowWidth > 360){
-		//image(broken_rock, windowWidth/2 - broken_rock.width/2, windowHeight/2 - broken_rock.height/4);
+	else if (iterations*20 >= windowHeight*3/5  && bounced == false && complete == false && windowHeight > 750 && windowWidth > 420){
 		bounced = true;
 	}
-	else if (bounced == true && complete == false && windowHeight > 640 && windowWidth > 360) {
+	else if (bounced == true && complete == false && windowHeight > 750 && windowWidth > 420) {
 		if (back_up <= windowHeight/20){
 			image(full_rock, windowWidth/2 - windowWidth/8, windowHeight*2/5 - back_up, windowWidth/4, windowHeight/3);
 			back_up += 10;
@@ -61,7 +60,7 @@ function draw() {
 			complete = true;
 		}
 	}
-	else if (complete == true && windowHeight > 640 && windowWidth > 360){
+	else if (complete == true && windowHeight > 750 && windowWidth > 420){
 		if(rock_diff>17) {
 			image(left_rock, windowWidth/2 - left_rock.width/6 - left_rock.width/rock_diff, windowHeight*3/4 - left_rock.height/6, left_rock.width/3, left_rock.height/3);
 			image(right_rock, windowWidth/2 - right_rock.width/6 + right_rock.width/rock_diff, windowHeight*3/4 - right_rock.height/6, right_rock.width/3, right_rock.height/3);
@@ -76,6 +75,8 @@ function draw() {
 			noLoop();
 	}
 	else {
+		image(left_rock, windowWidth/2 - left_rock.width/10 - left_rock.width/rock_diff, windowHeight*3/4 - left_rock.height/10, left_rock.width/5, left_rock.height/5);
+		image(right_rock, windowWidth/2 - right_rock.width/10 + right_rock.width/rock_diff, windowHeight*3/4 - right_rock.height/10, right_rock.width/5, right_rock.height/5);
 		image(aaveg, windowWidth/2 - aaveg.width/sizeVar, windowHeight/2 - aaveg.height/sizeVar, aaveg.width*2/sizeVar, aaveg.height*2/sizeVar);
 		image(carved, windowWidth/2 - carved.width/sizeVar, windowHeight*3/4 - carved.height/sizeVar, carved.width*2/sizeVar, carved.height*2/sizeVar);
 		if(sizeVar>10)
