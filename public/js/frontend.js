83323200afcc1a3496b7e7777ad18ea5d4aa213a
Tanvi Kumar <tanvikumar777@gmail.com>
2018-01-14 14:49:35 +0530

    Make more friendly for resizing.

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -22,6 +22,7 @@ function preload() {
     broken_rock = loadImage("imgs/broken_rock.png");
     aaveg = loadImage("imgs/Layer2.png");
     carved = loadImage("imgs/carved.png");
+    mobile_bg = loadImage("imgs/mobile_bg.png");
 }
 
 function setup() {
@@ -36,31 +37,37 @@ function setup() {
 
 function draw() {
 	iterations++;
+	clear();
 	fill(color(126, 166, 247));
     noStroke();
 	rect(0,0,windowWidth, windowHeight);
-	background(bg_image);
 	if(iterations*20 < windowHeight*3/5 && windowHeight > 750 && windowWidth > 420) {
+		background(bg_image);
 		image(full_rock, windowWidth/2 - windowWidth/8, iterations*20, windowWidth/4, windowHeight/3);
 		
 	}
 	else if (iterations*20 >= windowHeight*3/5  && bounced == false && complete == false && windowHeight > 750 && windowWidth > 420){
+		background(bg_image);
 		bounced = true;
 	}
 	else if (bounced == true && complete == false && windowHeight > 750 && windowWidth > 420) {
 		if (back_up <= windowHeight/20){
+			background(bg_image);
 			image(full_rock, windowWidth/2 - windowWidth/8, windowHeight*2/5 - back_up, windowWidth/4, windowHeight/3);
 			back_up += 10;
 		}
 		else if (back_down <= windowHeight/4){
+			background(bg_image);
 			image(full_rock, windowWidth/2 - windowWidth/8, windowHeight*2/5 - back_up + back_down, windowWidth/4, windowHeight/3);
 			back_down += 10;
 		}
 		else {
 			complete = true;
+			background(bg_image);
 		}
 	}
 	else if (complete == true && windowHeight > 750 && windowWidth > 420){
+		background(bg_image);
 		if(rock_diff>17) {
 			image(left_rock, windowWidth/2 - left_rock.width/6 - left_rock.width/rock_diff, windowHeight*3/4 - left_rock.height/6, left_rock.width/3, left_rock.height/3);
 			image(right_rock, windowWidth/2 - right_rock.width/6 + right_rock.width/rock_diff, windowHeight*3/4 - right_rock.height/6, right_rock.width/3, right_rock.height/3);
@@ -75,11 +82,12 @@ function draw() {
 			noLoop();
 	}
 	else {
-		image(left_rock, windowWidth/2 - left_rock.width/10 - left_rock.width/rock_diff, windowHeight*3/4 - left_rock.height/10, left_rock.width/5, left_rock.height/5);
-		image(right_rock, windowWidth/2 - right_rock.width/10 + right_rock.width/rock_diff, windowHeight*3/4 - right_rock.height/10, right_rock.width/5, right_rock.height/5);
+		background(mobile_bg);
+		image(left_rock, windowWidth/2 - left_rock.width/10 - left_rock.width/24, windowHeight*3/4 - left_rock.height/10, left_rock.width/5, left_rock.height/5);
+		image(right_rock, windowWidth/2 - right_rock.width/10 + right_rock.width/24, windowHeight*3/4 - right_rock.height/10, right_rock.width/5, right_rock.height/5);
 		image(aaveg, windowWidth/2 - aaveg.width/sizeVar, windowHeight/2 - aaveg.height/sizeVar, aaveg.width*2/sizeVar, aaveg.height*2/sizeVar);
 		image(carved, windowWidth/2 - carved.width/sizeVar, windowHeight*3/4 - carved.height/sizeVar, carved.width*2/sizeVar, carved.height*2/sizeVar);
-		if(sizeVar>10)
+		if(sizeVar>14)
 			sizeVar--;
 		else
 			noLoop();
@@ -88,5 +96,26 @@ function draw() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+	clear();
+    resizeCanvas(windowWidth, windowHeight);
+    fill(color(126, 166, 247));
+    noStroke();
+	rect(0,0,windowWidth, windowHeight);
+    if (windowHeight > 750 && windowWidth > 420){
+    	background(bg_image);
+  		image(left_rock, windowWidth/2 - left_rock.width/6 - left_rock.width/17, windowHeight*3/4 - left_rock.height/6, left_rock.width/3, left_rock.height/3);
+  		image(right_rock, windowWidth/2 - right_rock.width/6 + right_rock.width/17, windowHeight*3/4 - right_rock.height/6, right_rock.width/3, right_rock.height/3);
+  		image(aaveg, windowWidth/2 - aaveg.width/10, windowHeight/2 - aaveg.height/10, aaveg.width*2/10, aaveg.height*2/10);
+		image(carved, windowWidth/2 - carved.width/10, windowHeight*3/4 - carved.height/10, carved.width*2/10, carved.height*2/10);
+    }
+    else {
+    	fill(color(36, 113, 51));
+    	rect(0, windowHeight*5/6, windowWidth, windowHeight);
+    	background(mobile_bg);
+		image(left_rock, windowWidth/2 - left_rock.width/10 - left_rock.width/24, windowHeight*3/4 - left_rock.height/10, left_rock.width/5, left_rock.height/5);
+		image(right_rock, windowWidth/2 - right_rock.width/10 + right_rock.width/24, windowHeight*3/4 - right_rock.height/10, right_rock.width/5, right_rock.height/5);
+		image(aaveg, windowWidth/2 - aaveg.width/14, windowHeight/2 - aaveg.height/14, aaveg.width*2/14, aaveg.height*2/14);
+		image(carved, windowWidth/2 - carved.width/14, windowHeight*3/4 - carved.height/14, carved.width*2/14, carved.height*2/14);
+    }
+
 }
