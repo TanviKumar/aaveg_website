0fa0c01de930c6ed08615c7920d15bfb2f844232
paiakshay998 <paiakshay998@gmail.com>
2018-01-20 17:27:07 +0530

    modified photography page

--- resources/views/photography.blade.php
+++ resources/views/photography.blade.php
@@ -23,22 +23,9 @@
     <p class="lead">
 <p>Calling out all shutterbugs, insta-kings and queens alike.</p>
 <p>Frame the ultimate shot, to showcase your mastery of aperture and exposure, and win it for your hostel.</p>
-    </p>
-    <h2>Rules</h2>
-    <p class="lead">
-<ul>
-<li><span class="s2">There are no restrictions on the number of participants from each hostel.</span></li>
-<li><span class="s2">Each participant is allowed upto two entries</span></li>
-<li><span class="s2">Pictures for round one should be submitted by 23:59:59 January 31st, 2017</span></li>
-<li><span class="s2">Dates for round 2 would be intimated later.</span></li>
-<li><span class="s2">Pictures are to be taken only inside the Nitt campus.</span></li>
-<li><span class="s2">Photographs taken on any device (mobile, digital camera or DSLR) are accepted.</span></li>
-<li><span class="s2">Photos can be edited. If edited, then the participants must send the original photo along with the edited one.</span></li>
-<li><span class="s2">The exif data should be intact.</span></li>
-<li><span class="s2">If any of the rules are violated, then the participant will be disqualified.</span></li>
-<li><span class="s2">The entries will be judged on how far it conveys the theme given, caption, use of techniques, composition, framing.</span></li>
-</ul>
-    </p>
+<h2>Rulebook</h2>
+<p><a href="https://goo.gl/CJFovC"><span class="s4">Rules</span></a></p>
+	</p>
     <hr class="my-2">
     <form id="photography-form" enctype="multipart/form-data" method="POST">
   <div class="form-group">
