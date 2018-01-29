a6b49c48f9804bb5c4f4ab279e66ce931439f855
Tanvi Kumar <tanvikumar777@gmail.com>
2018-01-29 18:25:08 +0530

    Set team images

--- public/js/team.js
+++ public/js/team.js
@@ -132,78 +132,78 @@ function setTeam() {
        {
            "name":  "Aravind",
            "post":  "Content Writer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+"IMG­­­_8506.jpg"
        },
        {
            "name":  "Adil",
            "post":  "Content Writer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8454.jpg"
        },
        {
            "name":  "Ipsita",
            "post":  "Content Writer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8385.jpg"
        },
        {
            "name":  "Aindrila",
            "post":  "Content Writer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8408.jpg"
        },
        {
            "name":  "Swagata",
            "post":  "Content Writer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8423.jpg"
        },
        {
            "name":  "Harini",
            "post":  "Content Writer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+"IMG­­_8403.jpg"
        },
 
        {
            "name":  "Pooja",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8500.jpg"
        },
                {
            "name":  "Sanjana",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8406.jpg"
        },
        {
            "name":  "Ananthanarayanan",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8409.jpg"
        },
        {
            "name":  "Deepti",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8482.jpg"
        },
        {
            "name":  "Subash",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8412.jpg"
        },
        {
            "name":  "Aman",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8464.jpg"
        },
        {
            "name":  "Anand",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8453.jpg"
        },
        {
            "name":  "Vijay",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8434.jpg"
        },
        {
            "name":  "Evlin",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8430.jpg"
        },
        {
            "name":  "Nilesh",
@@ -218,67 +218,67 @@ function setTeam() {
        {
            "name":  "Srinivas",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8414.jpg"
        },
        {
            "name":  "Kishore",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8504.jpg"
        },
        {
            "name":  "Nitin",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8469.jpg"
        },
        {
            "name":  "Raj",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG­­­_8459.jpg"
        },
        {
            "name":  "Riddhi",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+"IMG­_8510.jpg"
        },
        {
            "name":  "Sanju",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+"IMG­_8424.jpg"
        },
        {
            "name":  "Swayamprava",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+"IMG_8512.jpg"
        },
        {
            "name":  "Rahul",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8383.jpg"
        },
        {
            "name":  "Adarsh",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8438.jpg"
        },
        {
            "name":  "Ayush",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8415.jpg"
        },
        {
            "name":  "Diya",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG_8508.jpg"
        },
        {
            "name":  "Kartikey",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8485.jpg"
        },
        {
            "name":  "Michelle",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG_8441.jpg"
        },
        {
            "name":  "Raj",
@@ -288,22 +288,22 @@ function setTeam() {
        {
            "name":  "Rengadeva",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8422.jpg"
        },
        {
            "name":  "Sahil",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8419.jpg"
        },
        {
            "name":  "Savita",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG_8432.jpg"
        },
        {
            "name":  "Uttish",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8427.jpg"
        },
        {
            "name":  "Vijay",
@@ -313,12 +313,12 @@ function setTeam() {
        {
            "name":  "George",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8401.jpg"
        },
        {
            "name":  "Kathirvel",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8377.jpg"
        },
        {
            "name":  "Aquif",
@@ -328,102 +328,102 @@ function setTeam() {
        {
            "name":  "Achyut",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8379.jpg"
        },
        {
            "name":  "Ajay",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8392.jpg"
        },
        {
            "name":  "Deeraj",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8384.jpg"
        },
        {
            "name":  "Dhwani",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG_8394.jpg"
        },
        {
            "name":  "Karthik",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8475.jpg"
        },
        {
            "name":  "Nandhini",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG_8396.jpg"
        },
        {
            "name":  "Shallypreethika",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG_8398.jpg"
        },
        {
            "name":  "Tamagno",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8428.jpg"
        },
        {
            "name":  "Amritha",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8491.jpg"
        },
        {
            "name":  "Vamsi",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8444.jpg"
        },
        {
            "name":  "Jaypal",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8467.jpg"
        },
        {
            "name":  "Kunal",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8478.jpg"
        },
        {
            "name":  "Saimothish",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8382.jpg"
        },
        {
            "name":  "Neel",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8477.jpg"
        },
        {
            "name":  "Venkat",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8511.jpg"
        },
        {
            "name":  "Raghuveer",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8411.jpg"
        },
        {
            "name":  "Kshiteesh",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8495.jpg"
        },
        {
            "name":  "Saeed",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8416.jpg"
        },
        {
            "name":  "Harsh Vora",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8418.jpg"
        },
        {
            "name":  "Karthi",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8387.jpg"
        },
        {
            "name":  "Meghna",
@@ -433,47 +433,47 @@ function setTeam() {
        {
            "name":  "Sahithi",
            "post":  "Organizer",
-           "image": base_url+"femaleplaceholder.jpg"
+           "image": base_url+" IMG_8407.jpg"
        },
        {
            "name":  " Vishnu",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8516.jpg"
        },
        {
            "name":  "Shailesh",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8517.jpg"
        },
        {
            "name":  "Santhosh",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8380.jpg"
        },
        {
            "name":  "Tarun",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8461.jpg"
        },
        {
            "name":  "Vaibhav",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8417.jpg"
        },
        {
            "name":  "Yash",
            "post":  "Organizer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+"IMG_8443.jpg"
        },
        {
            "name":  "Ragland",
            "post":  "Designer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8420.jpg"
        },
        {
            "name":  "Sathwik",
            "post":  "Designer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+"IMG_8375.jpg"
        },
        {
            "name":  "Aakash",
@@ -483,12 +483,12 @@ function setTeam() {
        {
            "name":  "Amogh",
            "post":  "Designer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+"IMG_8371.jpg"
        },
        {
            "name":  "Yagnesh",
            "post":  "Designer",
-           "image": base_url+"maleplaceholder.jpg"
+           "image": base_url+" IMG_8374.jpg"
        },
        {
            "name":  "Mohasin",
